53f85abc1127c86fe3138f619bb006bae2f18eb6
Kriok6 <rodrigohenriq1@gmail.com>
2026-03-16 16:56:27 -0300

    Remove scripts legados e ajusta header

--- database/produtos_estoque.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE produtos
-    ADD COLUMN controle_estoque TINYINT(1) NOT NULL DEFAULT 0 AFTER qtdEstoque,
-    ADD COLUMN estoque_minimo INT NULL AFTER controle_estoque,
-    ADD COLUMN ponto_compra INT NULL AFTER estoque_minimo;
